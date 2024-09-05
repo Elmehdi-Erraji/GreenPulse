677b7645717f3aaf38c27e91f257a73c51c02796
Mehdi <elmehdiworkemail@gmail.com>
2024-09-05 15:01:58 +0200

    adding a db connection and testing it

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import database.Database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        try {
+            Database database = Database.getInstance();
+
+            database.getConnection();
+//            UserService userService = new UserService();
+//            Menu menu = new Menu(userService);
+//            menu.start();
+//            database.closeConnection();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
